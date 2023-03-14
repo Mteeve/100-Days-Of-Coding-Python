bda8e01bb1b1d5bdc8b5ceafe323a93ea4ece6a9
Mteeve Brian <122893519+Mteeve@users.noreply.github.com>
2023-03-14 17:16:34 +0000

    First sentence in ("""__""")

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-#print ("Hello Mteeve
+print("""
 
-print("""Hello there!
-I am trying to learn Python
-for scratch 
-using Replit""")
+Mark
+14th March 2023
+I am signing up for Replit's 100 days of Python challange!
+I will make sure to spend some time every day coding along, for a minimum of 10mins a day.
+I'll be using Relplit, an amaizing online IDE so I can do it this from my phone wherever i happen to be. No excuses for not coding from the middle of the field!
+I am feeling ;-)
+You can follow my progress at replit.com/@Mteeve
+
+
+""")
